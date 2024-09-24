b7e2d2357d7036805168864dd81e420a69df0637
hussein257 <husseiniradukunda4@gmail.com>
2024-09-24 15:51:16 +0200

    Update

--- product-list-with-cart-main/products.js
+++ product-list-with-cart-main/products.js
@@ -1,4 +1,4 @@
-    // Get variables (Declarations) 
+    // Get variables (Declarations)
     let open_shoopin_cart = document.getElementById("open_shoopin_cart");
     let shoop_cart = document.querySelector(".shoop_cart");
     let close_cart = document.querySelector(".close_cart");
@@ -14,33 +14,30 @@
 
     const apiKey = "J28VOBvRkgP/Gg1K76zDGQ==4sCrkEaDL6FkQYmZ";
     const options = {
-        method: "GET",
-        headers: {
+    method: "GET",
+    headers: {
         "X-Api-Key": apiKey,
-        },
+    },
     };
-    const apiURL =
-        "https://api.api-ninjas.com/v1/dogs?name=golden retriever";
+    const apiURL = "https://api.api-ninjas.com/v1/dogs?name=golden retriever";
 
     const dataDogs = () => {
-        fetch(apiURL, options)
+    fetch(apiURL, options)
         .then((response) => {
-            return response.json();
+        return response.json();
         })
         .then((dataDog) => {
-            console.log(dataDog);
-            const imageDog = (document.getElementById("image").src =data[0].image_link);
+        console.log(dataDog);
+        const imageDog = (document.getElementById("image").src =
+            data[0].image_link);
         })
         .catch((Error) => console.log(Error));
     };
     dataDogs();
 
-
     open_shoopin_cart.onclick = () => {
     shoop_cart.classList.add("new_shoop_cart");
     };
-
-
 
     // The first value
     let data_array = [];
@@ -67,24 +64,24 @@
         const parentDiv = document.createElement("div");
         parentDiv.classList.add("products_content");
         parentDiv.innerHTML = `
-            <img src="${item.image.desktop}" alt="${item.name}" />
-            <div class="btn_content">
-            <button class="add_to_cart" onclick="add_to_cart(${index})">
-            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewbackground-colorBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
-            Add to Cart
-            </button>
-            <div class="quantity_btn">
-                <div class="minus"><i class="fa-solid fa-minus"></i></div>
-                <div class="quantity">0</div>
-                <div class="plus"><i class="fa-solid fa-plus"></i></div>
-            </div>
-            </div>
-            <div class="details">
-            <h4 class="category">${item.category}</h4>
-            <h3 class="name_products">${item.name}</h3>
-            <h2 class="price">$${item.price}</h2>
-            </div>
-        `;
+                <img src="${item.image.desktop}" alt="${item.name}" />
+                <div class="btn_content">
+                <button class="add_to_cart" onclick="add_to_cart(${index})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewbackground-colorBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
+                Add to Cart
+                </button>
+                <div class="quantity_btn">
+                    <div class="minus"><i class="fa-solid fa-minus"></i></div>
+                    <div class="quantity">0</div>
+                    <div class="plus"><i class="fa-solid fa-plus"></i></div>
+                </div>
+                </div>
+                <div class="details">
+                <h4 class="category">${item.category}</h4>
+                <h3 class="name_products">${item.name}</h3>
+                <h2 class="price">$${item.price}</h2>
+                </div>
+            `;
         products_container.appendChild(parentDiv);
         });
     }
@@ -109,23 +106,22 @@
     btn.querySelector(".plus").onclick = () => {
         let quantity = btn.querySelector(".quantity");
         let num_quantity = parseInt(quantity.textContent);
-        
+
         count++;
         document.querySelector(".quantity_value").innerHTML = count;
         document.querySelector(
-            ".title_cart_shooping"
+        ".title_cart_shooping"
         ).innerHTML = `Your cart (${count})`;
 
         quantity.textContent = num_quantity + 1;
-        if (!newArray[index]) {1;
+        if (!newArray[index]) {
+        1;
         newArray[index] = { ...data_array[index], quantity: 1 };
         } else {
         newArray[index].quantity++;
         }
         creatProductList();
     };
-
-
 
     btn.querySelector(".minus").onclick = () => {
         let quantity = btn.querySelector(".quantity");
@@ -134,9 +130,9 @@
         count--;
         document.querySelector(".quantity_value").innerHTML = count;
         document.querySelector(
-            ".title_cart_shooping"
+        ".title_cart_shooping"
         ).innerHTML = `Your cart (${count})`;
-        
+
         if (num_quantity > 1) {
         quantity.textContent = num_quantity - 1;
         newArray[index].quantity--;
@@ -155,7 +151,6 @@
     };
     };
 
-    
     // creat products list ========
     const creatProductList = () => {
     shoop_container.innerHTML = "";
@@ -165,18 +160,20 @@
         const parentDiv = document.createElement("div");
         parentDiv.classList.add("shoop_content");
         parentDiv.innerHTML = `
-                <div class="text_shoop">
-                    <h3 class="name_shoop">${item.name}</h3>
-                        <div class="quantity_price_total">
-                            <h5 class="quantity_shoop">${item.quantity} x</h5>
-                            <div class="shooop_price_and_totale_shoop">
-                                <h5 class="price_shoop">@ $${item.price}</h5>
-                                <h5 class="totale_shoop">$${item.price * item.quantity}</h5>
-                            </div>
+                    <div class="text_shoop">
+                        <h3 class="name_shoop">${item.name}</h3>
+                            <div class="quantity_price_total">
+                                <h5 class="quantity_shoop">${item.quantity} x</h5>
+                                <div class="shooop_price_and_totale_shoop">
+                                    <h5 class="price_shoop">@ $${item.price}</h5>
+                                    <h5 class="totale_shoop">$${
+                                    item.price * item.quantity
+                                    }</h5>
+                                </div>
+                        </div>
                     </div>
-                </div>
-                <i class="fa-solid fa-xmark" onclick="removeProduct(${index})"></i>
-        `;
+                    <i class="fa-solid fa-xmark" onclick="removeProduct(${index})"></i>
+            `;
         shoop_container.appendChild(parentDiv);
         totalePrice += item.price * item.quantity;
         totale_shoop_price.innerHTML = "$" + totalePrice;
@@ -184,7 +181,6 @@
     }
     };
 
-    
     const removeProduct = (index) => {
     newArray.splice(index, 1);
     creatProductList();
@@ -216,36 +212,36 @@
     order_container.style.display = "flex";
     finel_order_container.innerHTML = "";
     totale_order.innerHTML = "";
-    if(newArray.length>1){
-    start_new_order.style.display = "block";
-    newArray.forEach((item, index) => {
+    if (newArray.length > 1) {
+        start_new_order.style.display = "block";
+        newArray.forEach((item, index) => {
         const parenElement = document.createElement("div");
         parenElement.classList.add("only_order");
         parenElement.innerHTML = `
-        <div class="new-details">
-                <img src="${item.image.desktop}" alt="${item.name}">
-                <div class="textOrder">
-                    <h3>${item.name}</h3>
-                    <div class="details_price">
-                    <p class="quantity">${item.quantity}x</p>
-                    <h4 class="price">$${item.price}</h4>
+            <div class="new-details">
+                    <img src="${item.image.desktop}" alt="${item.name}">
+                    <div class="textOrder">
+                        <h3>${item.name}</h3>
+                        <div class="details_price">
+                        <p class="quantity">${item.quantity}x</p>
+                        <h4 class="price">$${item.price}</h4>
+                        </div>
                     </div>
-                </div>
-                
-                <h2>$${item.quantity * item.price}</h2>
-        </div>
-    `;
+                    
+                    <h2>$${item.quantity * item.price}</h2>
+            </div>
+        `;
         finel_order_container.appendChild(parenElement);
         totale_order.innerHTML =
-        newArray.reduce (
+            newArray.reduce(
             (totale, item) => totale + (item ? item.quantity * item.price : 0),
             0
-        ) + "$";
-    });
-    }else{
-    start_new_order.style.display = "none";
-    finel_order_container.innerHTML =
-    '<p class="error_order">oh sorry you need to order somthing </p>';
+            ) + "$";
+        });
+    } else {
+        start_new_order.style.display = "none";
+        finel_order_container.innerHTML =
+        '<p class="error_order">oh sorry you need to order somthing </p>';
     }
     };
 
@@ -261,19 +257,17 @@
     start_new_order.style.display = "none";
     finel_order_container.innerHTML =
         '<p class="thanks_order">thank you for your order <img src="images/cart_add_new_shop_ecommerce_icon_150698.png" alt=""/></p>';
-        count=0;
-        document.querySelector(".quantity_value").innerHTML = count;
-        document.querySelector(
+    count = 0;
+    document.querySelector(".quantity_value").innerHTML = count;
+    document.querySelector(
         ".title_cart_shooping"
-        ).innerHTML = `Your cart (${count})`;
-        totale_shoop_price.innerHTML = "$" + 0;
-        let products_content = document.querySelectorAll(".products_content");
-        products_content.forEach((item) => {
+    ).innerHTML = `Your cart (${count})`;
+    totale_shoop_price.innerHTML = "$" + 0;
+    let products_content = document.querySelectorAll(".products_content");
+    products_content.forEach((item) => {
         let quantity_btn = item.querySelector(".quantity_btn");
         let add_button = item.querySelector(".add_to_cart");
         quantity_btn.style.display = "none";
         add_button.style.display = "block";
-        });
+    });
     };
-
-    
